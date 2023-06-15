c353dcba3de2e5a5d0d204eea07f7548725b3f30
Purityndirangu <ndiranguwanjikupurity@gmail.com>
2023-06-15 17:31:32 +0300

    validate adding contacts page

--- app/src/main/java/com/collections/contacts/MainActivity.kt
+++ app/src/main/java/com/collections/contacts/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.collections.contacts
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.collections.contacts.databinding.ActivityMainBinding
 
@@ -11,6 +13,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val addContactButton = findViewById<Button>(R.id.FABAddContact)
+        addContactButton.setOnClickListener {
+            val intent = Intent(this, inputcontact::class.java)
+            startActivity(intent)
+        }
+
     }
 
     override fun onResume() {
@@ -18,16 +27,20 @@ class MainActivity : AppCompatActivity() {
         displayContacts()
     }
     fun displayContacts(){
-        var contacts1 = Contacts("","Njoki Dee","254672","dee@yahoo.com")
-        var contacts2 = Contacts("","Bill Gates","254789","dee@yahoo.com")
-        var contacts3 = Contacts("","Njoki Dee","254656","dee@yahoo.com")
-        var contacts4 = Contacts("","Njoki Dee","254672","dee@yahoo.com")
-        var contacts5 = Contacts("","Njoki Dee","254672","dee@yahoo.com")
+        var contacts1 = Contacts("https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1434&q=80","Njoki Dee","254672","dee@yahoo.com")
+        var contacts2 = Contacts("https://images.unsplash.com/photo-1517423568366-8b83523034fd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80","Bill Gates","254789","dee@yahoo.com")
+        var contacts3 = Contacts("https://images.unsplash.com/photo-1530041539828-114de669390e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80","Njoki Dee","254656","dee@yahoo.com")
+        var contacts4 = Contacts("https://images.unsplash.com/photo-1517519014922-8fc06b814a0e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=436&q=80","Njoki Dee","254672","dee@yahoo.com")
+        var contacts5 = Contacts("https://images.unsplash.com/photo-1523626797181-8c5ae80d40c2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=435&q=80","Njoki Dee","254672","dee@yahoo.com")
         var lists = listOf<Contacts>(contacts1,contacts2,contacts3,contacts4,contacts5)
 
         binding.rvTweet.layoutManager = LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
         val namesAdapter= ContactRvAdapter(lists)
         binding.rvTweet.adapter=namesAdapter
+
+
     }
+
+
 
 }
--- app/src/main/java/com/collections/contacts/contactRvAdapter.kt
+++ app/src/main/java/com/collections/contacts/contactRvAdapter.kt
@@ -5,6 +5,8 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.collections.contacts.databinding.ActivityMainBinding
 import com.collections.contacts.databinding.ContactListBinding
+import com.squareup.picasso.Picasso
+import jp.wasabeef.picasso.transformations.CropCircleTransformation
 
 class ContactRvAdapter(var contactList:List<Contacts>):RecyclerView.Adapter<ContactViewHolder>(){
 
@@ -15,16 +17,26 @@ class ContactRvAdapter(var contactList:List<Contacts>):RecyclerView.Adapter<Cont
 
     override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
         var item=contactList.get(position)
-        holder.binding.ivAvatar.toString()
+        var binding = holder.binding
+//        holder.binding.ivAvatar.toString()
         holder.binding.tvName.text=item.fullnames
         holder.binding.tvPhoneNumber.text=item.phoneNumber
         holder.binding.tvEmail.text=item.emailaddress
+        Picasso
+            .get()
+            .load(item.avatar)
+            .resize(80,80)
+            .centerCrop()
+            .transform(CropCircleTransformation())
+            .into(binding.ivAvatar)
     }
 
     override fun getItemCount(): Int {
         return contactList.size
     }
 }
+
+
 
 class  ContactViewHolder(var binding: ContactListBinding):RecyclerView.ViewHolder(binding.root){
 
